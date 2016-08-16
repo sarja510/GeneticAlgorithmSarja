fc8cf9ee2d384e7f900518010be83058cc8772fe
sarja <asif.ahmed.sarja@gmail.com>
2016-08-16 15:37:05 +0600

    Initial commit

---
+++ src/main/java/VRPConfiguration.java
@@ -0,0 +1,5 @@
+/**
+ * Created by sarja on 8/16/2016.
+ */
+public class VRPConfiguration {
+}
---
+++ src/main/java/VRPFitness.java
@@ -0,0 +1,13 @@
+/**
+ * Created by sarja on 8/16/2016.
+ */
+
+import org.jenetics.BitChromosome;
+import org.jenetics.BitGene;
+import org.jenetics.Genotype;
+import org.jenetics.engine.Engine;
+import org.jenetics.engine.EvolutionResult;
+import org.jenetics.util.Factory;
+
+public class VRPFitness {
+}
